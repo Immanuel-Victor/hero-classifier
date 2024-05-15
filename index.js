e3f12a92d5c98dd493b1b12d91d24819ac7f3ea8
Immanuel-Victor <immanuelvictor.a.f@gmail.com>
2024-05-15 20:38:16 -0300

    feat: adiciona lógica da ativiadade

---
+++ index.js
@@ -0,0 +1,24 @@
+const heroName = "Immanuel";
+let points = 2000;
+let elos = ["Ferro", "Bronze", "Prata", "Ouro", "Platina", "Ascendente", "Imortal", "Radiante"]
+let finalPhrase = ''
+
+if(points <= 1000) {
+  finalPhrase = 'O herói de nome ' + heroName + ' está no nível de ' + elos[0];
+} else if (1000 < points <= 2000 ) {
+   finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[1];
+} else if (2000 < points <= 5000 ) {
+   finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[2];
+} else if (5000 < points <= 7000 ) {
+   finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[3];
+} else if (7000 < points <= 8000 ) {
+   finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[4];
+} else if (8000 < points <= 9000 ) {
+   finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[5];
+} else if (9000 < points <= 10000 ) {
+   finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[6];
+} else {
+   finalPhrase =  'O herói de nome  ' + heroName +  'está no nível de ' + elos[7];
+}
+
+console.log(finalPhrase)
