23bccf97ab0ea78dc35f2e3ea37f42d09203db80
Immanuel Victor <84516493+Immanuel-Victor@users.noreply.github.com>
2024-05-16 20:22:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ let finalPhrase = ''
 
 if(points <= 1000) {
   finalPhrase = 'O herói de nome ' + heroName + ' está no nível de ' + elos[0];
-} else if (1000 < points <= 2000 ) {
+} else if (1000 < points && points <= 2000 ) {
    finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[1];
-} else if (2000 < points <= 5000 ) {
+} else if (2000 < points && points <= 5000 ) {
    finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[2];
-} else if (5000 < points <= 7000 ) {
+} else if (5000 < points && points  <= 7000 ) {
    finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[3];
-} else if (7000 < points <= 8000 ) {
+} else if (7000 < points && points  <= 8000 ) {
    finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[4];
-} else if (8000 < points <= 9000 ) {
+} else if (8000 < points && points  <= 9000 ) {
    finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[5];
-} else if (9000 < points <= 10000 ) {
+} else if (9000 < points && points  <= 10000 ) {
    finalPhrase =  'O herói de nome ' + heroName + ' está no nível de ' + elos[6];
 } else {
    finalPhrase =  'O herói de nome  ' + heroName +  'está no nível de ' + elos[7];
